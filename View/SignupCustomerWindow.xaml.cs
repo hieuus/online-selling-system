31f731c6738142fd0d8cf6a577ee0e581093ec78
Hieu Nguyen <nguyenvanhieu.gv2002@gmail.com>
2022-12-26 01:22:19 +0700

    customer sign up success

--- View/SignupCustomerWindow.xaml.cs
+++ View/SignupCustomerWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Window = System.Windows.Window;
 
 namespace OnlineSellingSystem.View
 {
@@ -45,5 +49,88 @@ namespace OnlineSellingSystem.View
             screen.ShowDialog();
         }
 
+        private void customerSignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            string cusName = fullName.Text;
+            string cusEmail = email.Text;
+            string cusPhone = phone.Text;
+
+            string cusNoRoad = houseNumber.Text;
+            string cusRoad = road.Text;
+            string cusWard = ward.Text;
+            string cusDistrict = distric.Text;
+            string cusCity = city.Text;
+
+            //Connect database
+            var _connection = new SqlConnection("server=.; database=OnlineSellingDatabase;Trusted_Connection=yes;MultipleActiveResultSets=true");
+            _connection.Open();
+
+            string sp_insertCustomer = "sp_insertCustomer";
+            var command = new SqlCommand(sp_insertCustomer, _connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@CustomerName",
+                SqlDbType = System.Data.SqlDbType.NChar,
+                Value = cusName
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@CustomerPhone",
+                SqlDbType = System.Data.SqlDbType.Char,
+                Value = cusPhone
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@CustomerEmail",
+                SqlDbType = System.Data.SqlDbType.Char,
+                Value = cusEmail
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@CustomerAddressNoR",
+                SqlDbType = System.Data.SqlDbType.Int,
+                Value = cusNoRoad
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@CustomerAddressRoad",
+                SqlDbType = System.Data.SqlDbType.NChar,
+                Value = cusRoad
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@CustomerAddressWard",
+                SqlDbType = System.Data.SqlDbType.NChar,
+                Value = cusWard
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@CustomerAddressDistrict",
+                SqlDbType = System.Data.SqlDbType.NChar,
+                Value = cusDistrict
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@CustomerAddressCity",
+                SqlDbType = System.Data.SqlDbType.NChar,
+                Value = cusCity
+            });
+
+            int isSuccess = command.ExecuteNonQuery();
+
+            if(isSuccess == 1)
+            {
+                MessageBox.Show($"Success! You can log in with your phone: {cusPhone} and default password: 123456");
+                move_to_signin(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+
+
+        }
     }
 }
